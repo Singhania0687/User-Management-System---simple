59c76c961ab375838be2f832e87b302717f9dfd1
Abhishek Kumar <Abhishekundefeated0687@gmail.com>
2025-09-26 00:14:21 +0530

    added Mongo_URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,14 @@
 const mongoose=require('mongoose')
+const dotenv=require('dotenv').config()
 async function connectDB(){
+     const uri=process.env.MONGO_URI
     try{
+       
          await mongoose.connect(process.env.MONGO_URI)
             console.log("Database connected successfully")
     }
     catch(err){
-        console.log("Error in Db connection",err)
+        console.log("Error in Db connection",uri)
     }
    
 
